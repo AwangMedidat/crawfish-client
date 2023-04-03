7271c825359940fe85cb2ad673655d05b4356fca
AwangMedidat <awangmedidat98@gmail.com>
2023-04-03 14:42:51 +0700

    add-bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '@/styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.css'
+// import "bootstrap/dist/js/bootstrap.bundle.min";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
